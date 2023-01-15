e64ef6f2ad24e2de5c590bd66c0b75cf95443f2a
Revant Nandgaonkar <revant.one@gmail.com>
2023-01-16 04:21:34 +0530

    refactor: use single erpnext image (#148)
    * chore: resolve merge conflict
    
    * ci: fix test use updated mariadb chart
    
    * fix: readme url in install notes
    
    * test: update ingress controller
    
    * fix: use ingressClass if set in values
    
    * docs: update release guide
    
    * ci: test using docker-compose
    
    * test: do not fix volume for faster config

--- tests/script.sh
+++ tests/script.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-echo -e "\e[1m\e[4mCreate testcluster with k3d and kubernetes/ingress-nginx\e[0m"
-k3d cluster create testcluster --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer --k3s-arg "--disable=traefik@server:0"
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
+echo -e "\e[1m\e[4mInstall kubectl\e[0m"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
+
+echo -e "\e[1m\e[4mInstall Helm\e[0m"
+curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+echo -e "\e[1m\e[4mConfigure K3s\e[0m"
+mkdir -p /root/.kube
+curl k3s:8081 -o /root/.kube/config
+chmod go-r /root/.kube/config
+export KUBECONFIG=/root/.kube/config
+kubectl cluster-info
+
+echo -e "\e[1m\e[4mInstall kubernetes/ingress-nginx\e[0m"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml
 echo -e "\n"
 
 echo -e "\e[1m\e[4mAdd Helm Repositories\e[0m"
@@ -14,7 +27,7 @@ echo -e "\n"
 
 echo -e "\e[1m\e[4mCreate mariadb release from bitnami/mariadb helm chart\e[0m"
 kubectl create namespace mariadb
-helm install mariadb -n mariadb bitnami/mariadb -f tests/mariadb/values.yaml --version 10.3.3 --wait
+helm install mariadb -n mariadb bitnami/mariadb -f tests/mariadb/values.yaml --version 11.4.2 --wait
 echo -e "\n"
 
 echo -e "\e[1m\e[4mCreate in-cluster release from nfs-ganesha-server-and-external-provisioner/nfs-server-provisioner helm chart\e[0m"
@@ -42,7 +55,7 @@ kubectl -n erpnext wait --timeout=1800s --for=condition=complete jobs --all
 echo -e "\n"
 
 echo -e "\e[1m\e[4mPing mysite.localhost (upstream chart)\e[0m"
-curl -sS http://mysite.localhost/api/method/ping
+curl -sS -H "Host: mysite.localhost" http://k3s/api/method/ping
 echo -e "\n"
 
 echo -e "\e[1m\e[4mUpgrade frappe-bench release with chart from PR\e[0m"
@@ -50,5 +63,15 @@ helm upgrade frappe-bench -n erpnext erpnext -f tests/erpnext/values.yaml --wait
 echo -e "\n"
 
 echo -e "\e[1m\e[4mPing mysite.localhost (pr chart)\e[0m"
-curl -sS http://mysite.localhost/api/method/ping
+curl -sS -H "Host: mysite.localhost" http://k3s/api/method/ping
 echo -e "\n"
+
+echo -e "\e[1m\e[4mCleanup\e[0m"
+kubectl delete ingress -n erpnext --all
+kubectl delete jobs -n erpnext --all
+helm delete --wait -n erpnext frappe-bench
+helm delete --wait -n mariadb mariadb
+helm delete --wait -n nfs in-cluster
+kubectl delete pvc -n erpnext --all
+kubectl delete pvc -n mariadb --all
+kubectl delete pvc -n nfs --all
